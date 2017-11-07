b905031bbbe465af368c0576ca40d4b48c3b981f
fnecas <florianecas@gmail.com>
2017-11-07 19:50:02 +0100

    Cloud Vision Working
    History updated

--- HISTORY.md
+++ HISTORY.md
@@ -1,7 +1,5 @@
 # 06/11
 ## Preparation
-
-Jérôme : 
 * Paramètrage PostgreSQL + Heroku.
 * Liaison dépot Github et Heroku pour déploiement rapide. 
 * Serveur basique de départ avec routes suivante :
@@ -10,23 +8,26 @@ Jérôme :
 	* Reset la base de donnée.
 * Recherches Kubernetes
 
-Florian :
 * Mise en place du detecteur de beacon
 * Tentative d'afficher le beacon sur la mainActivity
 
 # 07/11
-Jérôme :
-Matin - Tenter desespérement de faire fonctionner l'authentification vers google. (Sans Succés)
+##Android
+* Modification du serveur Web :
+	* Actualisation et tests de la route pour effectuer une inscription
+	* Premier pas d'Intégration serveur/android OK : J'appuis sur un bouton de l'interface (téléphone), le téléphone envoi un POST, l'user si il n'existe pas est ajouté dans la base (verifié) et le serveur renvoi un JSON OK.
+* Application Android :
+	* Ajout d'une permission INTERNET dans le manifest. (Pb rencontré : PERMISSION DENIED)
+	* Ajout bouton sur l'interface
+	* Connecter le bouton à la fonction de Post un user(Fait par Flo)
+	* Tests
 
-Après-midi - Switch sur l'application Android + Serveur Web avec objectif de début d'intégration :
-	* Modification du serveur Web :
-		* Actualisation et tests de la route pour effectuer une inscription
-		* Premier pas d'Intégration serveur/android OK : J'appuis sur un bouton de l'interface (téléphone), le téléphone envoi un POST, l'user si il n'existe pas est ajouté dans la base (verifié) et le serveur renvoi un JSON OK.
-	* Application Android :
-		* Ajout d'une permission INTERNET dans le manifest. (Pb rencontré : PERMISSION DENIED)
-		* Ajout bouton sur l'interface
-		* Connecter le bouton à la fonction de Post un user(Fait par Flo)
-		* Tests
+* Mise à jour de la main activity pour qu'elle contienne le barcode & beacon
+* Communique avec le serveur heroku (creation d'un utilisateur)
+
+##Backend
+* Matin - Tenter desespérement de faire fonctionner l'authentification vers google. (Sans Succés)
+* Google Cloud Vision (enfin!) mis en place
 
 A GARDER : 
 Tuto activer beacon sur Raspberry => https://learn.adafruit.com/pibeacon-ibeacon-with-a-raspberry-pi?view=all
--- Module_googlevision/main.py
+++ Module_googlevision/main.py
@@ -1,27 +1,46 @@
-from google.cloud import vision
-import google.oauth2.credentials
+# [START import_libraries]
+import argparse
+import base64
+
+import googleapiclient.discovery
+from google.oauth2 import service_account
+# [END import_libraries]
 
 
-credentials = google.oauth2.oauth2.credentials.Credentials('access_token',refresh_token='refresh_token',token_uri='token_uri',client_id='client_id',client_secret='client_secret')
+def main(photo_file):
+    """Run a label request on a single image"""
+
+    # [START authenticate]
+    credentials = service_account.Credentials.from_service_account_file('project-1a95fbdfa238.json')
+    service = googleapiclient.discovery.build('vision', 'v1', credentials=credentials)
+    # [END authenticate]
+
+    # [START construct_request]
+    with open(photo_file, 'rb') as image:
+        image_content = base64.b64encode(image.read())
+        service_request = service.images().annotate(body={
+            'requests': [{
+                'image': {
+                    'content': image_content.decode('UTF-8')
+                },
+                'features': [{
+                    'type': 'LABEL_DETECTION',
+                    'maxResults': 1
+                }]
+            }]
+        })
+        # [END construct_request]
+        # [START parse_response]
+        response = service_request.execute()
+        label = response['responses'][0]['labelAnnotations'][0]['description']
+        print('Found label: %s for %s' % (label, photo_file))
+        # [END parse_response]
 
 
-#client = Client(credentials=creds)
-
-#client = vision.Client()
-
-autorisation = vision.from_service_account_json('C:/Users/Jerome/Desktop/try-apis-bfc045e18c6d.json')
-client = vision.ImageAnnotatorClient(credentials=credentials)
-response = client.annotate_image({ 'image': {'source': {'image_uri': 'http://dinoanimals.pl/wp-content/uploads/2013/04/Lama-lamy-DinoAnimals.pl-1.jpg'}},'features': [{'type': vision.enums.Feature.Type.FACE_DETECTION}],})
-print(len(response.annotations))
-
-
-
-#client = vision.Client()
-#credentials = service_account.Credentials.from_service_account_file('C:/Users/Jerome/Desktop/try-apis-bfc045e18c6d.json')
-#scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/cloud-platform'])
-#credentials = credentials(scoped_credentials)
-
-#client = datastore.Client()
-#client = from_service_account_json(' C:/Users/Jerome/Desktop/try-apis-bfc045e18c6d.json')
-#GOOGLE_APPLICATION_CREDENTIALS='C:/Users/Jerome/Desktop/try-apis-bfc045e18c6d.json'
-
+# [START run_application]
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image_file', help='The image you\'d like to label.')
+    args = parser.parse_args()
+    main(args.image_file)
+# [END run_application]
