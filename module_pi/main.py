fa4d5f9f68ceb5d1518beffd066d21100f2027d2
jeromebonnet <jerome.bonnet@imerir.com>
2017-11-08 10:05:48 +0000

    modif

--- module_pi/main.py
+++ module_pi/main.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/toto')
 def index():
 	sense.show_message(":)",text_colour=(255, 0, 0))
 	sense.show_message(":x",text_colour=(0, 255, 0))
@@ -14,6 +14,6 @@ def index():
 
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='127.0.0.1')
 
 
