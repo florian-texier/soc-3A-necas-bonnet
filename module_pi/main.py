8023ad94d308f008b52d3afb470596bcec381038
jbonnet <jerome.bonnet@imerir.com>
2017-11-08 10:24:04 +0100

    hellow world du sense

--- module_pi/main.py
+++ module_pi/main.py
@@ -1,24 +1,8 @@
 from sense_hat import SenseHat
 sense = SenseHat()
-sense.show_message(":)")
-sense.flip_h()
-sense.show_message(":x")
-sense.rotation = 180
-sense.show_message(":(")
+sense.show_message(":)",text_colour=(255, 0, 0))
 
-# examples using (x, y, r, g, b)
-sense.set_pixel(0, 0, 255, 0, 0)
-sense.set_pixel(0, 7, 0, 255, 0)
-sense.set_pixel(7, 0, 0, 0, 255)
-sense.set_pixel(7, 7, 255, 0, 255)
 
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0, 0, 255)
+sense.show_message(":x",text_colour=(0, 255, 0))
 
-# examples using (x, y, pixel)
-sense.set_pixel(0, 0, red)
-sense.set_pixel(0, 0, green)
-sense.set_pixel(0, 0, blue)
-
-sense.show_message("MDR")
+sense.show_message(":(",text_colour=(0, 0, 255))
