2f8f661f381944480688ffdda787541629f73909
jbonnet <jerome.bonnet@imerir.com>
2017-11-08 10:52:30 +0100

    tt

--- module_pi/main.py
+++ module_pi/main.py
@@ -1,8 +1,22 @@
 from sense_hat import SenseHat
-sense = SenseHat()
-sense.show_message(":)",text_colour=(255, 0, 0))
 
+# -*- coding: utf-8 -*-
+import json, random
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+from db import Db
 
-sense.show_message(":x",text_colour=(0, 255, 0))
+app = Flask(__name__)
+app.debug = True
+CORS(app)
 
-sense.show_message(":(",text_colour=(0, 0, 255))
+#Vider la base
+@app.route('/reset',methods=['GET'])
+def reset():
+	sense = SenseHat()
+
+	sense.show_message(":)",text_colour=(255, 0, 0))
+	sense.show_message(":x",text_colour=(0, 255, 0))
+	sense.show_message(":(",text_colour=(0, 0, 255))
+    return "Reset done."
+
