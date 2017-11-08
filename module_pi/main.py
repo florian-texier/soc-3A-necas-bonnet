bfed9b0a5bf06d146475c5efd8c82c6545d0b1c1
jbonnet <jerome.bonnet@imerir.com>
2017-11-08 10:59:39 +0100

    tt

--- module_pi/main.py
+++ module_pi/main.py
@@ -1,21 +1,19 @@
 from sense_hat import SenseHat
 
-# -*- coding: utf-8 -*-
-import json, random
-from flask import Flask, request
-from flask_cors import CORS, cross_origin
-from db import Db
+from flask import Flask
 
 app = Flask(__name__)
-app.debug = True
-CORS(app)
 
-#Vider la base
-@app.route('/reset',methods=['GET'])
-def reset():
-	sense = SenseHat()
+
+@app.route('/')
+def index():
 	sense.show_message(":)",text_colour=(255, 0, 0))
 	sense.show_message(":x",text_colour=(0, 255, 0))
 	sense.show_message(":(",text_colour=(0, 0, 255))
-	return "Reset done."
+	return 'Hello world'
+
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0')
+
 
