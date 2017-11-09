257d0b63b359b92f8acceec4e1ae3a5abd1dbc0e
jeromebonnet <jerome.bonnet@imerir.com>
2017-11-09 17:05:15 +0000

    Pi : Fonctionne avec le serveur : Affiche numero d'équipe + status (OK/NOT) + Une seule fois ! (PRET pour démo non formel :DD

--- module_pi/main.py
+++ module_pi/main.py
@@ -5,13 +5,20 @@ from flask import Flask
 valeur = 50
 sense = SenseHat()
 while  (1) :
-	r = requests.get('http://polar-bayou-90643.herokuapp.com/test')
-	print(r.text)
-	if (r.text == 'a') : 
-		sense.show_message(":)",text_colour=(0, 255, 0))
+	etats = requests.get('http://polar-bayou-90643.herokuapp.com/lireetat')
+	t = requests.get('http://polar-bayou-90643.herokuapp.com/resetetat')
+	print(etats.json())
+	toto = etats.json()
+	print(toto['objet'][1])
+	etats=toto['objet']
+	for etat in range(0,len(etats)):
+		if (etats[etat] == 'a') : 
+			sense.show_message('%s'%(etat), text_colour=(0,255,0))
+			sense.show_message(":)",text_colour=(0, 255, 0))
 
-	if (r.text =='b') :
-		sense.show_message(":(",text_colour=(0, 0, 255))
-	if(r.text =='c'):
-		sense.show_message(":x",text_colour=(255,0,0))
+		if (etats[etat] =='b') :
+			sense.show_message('%s'%(etat), text_colour=(0,0,255))
+			sense.show_message(":(",text_colour=(0, 0, 255))
+		print(etats[etat])
 	time.sleep(1)
+
