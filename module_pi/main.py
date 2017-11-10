abd8a0a2632102499d9326b03ce90f16f10cd4c3
jeromebonnet <jerome.bonnet@imerir.com>
2017-11-10 09:03:00 +0000

    Correction bug numero equipe aleatoire dans le json

--- module_pi/main.py
+++ module_pi/main.py
@@ -7,18 +7,17 @@ sense = SenseHat()
 while  (1) :
 	etats = requests.get('http://polar-bayou-90643.herokuapp.com/lireetat')
 	t = requests.get('http://polar-bayou-90643.herokuapp.com/resetetat')
-	print(etats.json())
 	toto = etats.json()
-	print(toto['objet'][1])
 	etats=toto['objet']
 	for etat in range(0,len(etats)):
-		if (etats[etat] == 'a') : 
-			sense.show_message('%s'%(etat), text_colour=(0,255,0))
+		if (etats[etat]['etat'] == 'a') : 
+			sense.show_message('%s'%(etats[etat]['id']), text_colour=(0,255,0))
 			sense.show_message(":)",text_colour=(0, 255, 0))
 
-		if (etats[etat] =='b') :
-			sense.show_message('%s'%(etat), text_colour=(0,0,255))
+		if (etats[etat]['etat'] =='b') :
+			sense.show_message('%s'%(etats[etat]['id']), text_colour=(0,0,255))
 			sense.show_message(":(",text_colour=(0, 0, 255))
 		print(etats[etat])
-	time.sleep(1)
+	print(' ')
+	time.sleep(10)
 
