88e544f01fbdd659e327daab3507ccb2984790b9
jeromebonnet <jerome.bonnet@imerir.com>
2017-11-10 13:31:42 +0000

    Correction bug indentation

--- module_pi/main.py
+++ module_pi/main.py
@@ -7,13 +7,16 @@ sense = SenseHat()
 while  (1) :
 	#Je fais une requête pour récupérer tous les états des équipes 
 	etats = requests.get('http://polar-bayou-90643.herokuapp.com/lireetats')
-	
+
 	#Je fais une requête pour reset les états des équipes (Pour ne pas re-afficher)
 	r = requests.get('http://polar-bayou-90643.herokuapp.com/resetetats')
 
-	#Je stocke le tableau d'états
-	etats=etats.json()['objet']
-	
+	#Je stocke la reponse et la json dumps
+	temporaire=etats.json()
+
+	#Je  stocke uniquement le tableau d'états
+	etats = temporaire['objet']
+
 	#Pour chaque état, si j'ai un succés ou un echec et j'affiche sur la Pi :
 	for i in range(0,len(etats)):
 		if (etats[i]['etat'] == 'imgsuccess') : 
@@ -25,7 +28,7 @@ while  (1) :
 			sense.show_message(":(",text_colour=(0, 0, 255))
 		print(etats[i])
 	print(' ')
-	
+
 	#Je fais un sleep pour ne pas harceler de manière violente le serveur
 	time.sleep(10)
 
