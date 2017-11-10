c6c51c191044cab09f4bc47a998dfc00b96505e2
fnecas <florianecas@gmail.com>
2017-11-10 14:56:48 +0100

    Change to postgre

--- serveur_web/db-test.sql
+++ serveur_web/db-test.sql
@@ -1,35 +1,36 @@
-#------------------------------------------------------------
-#        Script MySQL.
-#------------------------------------------------------------
+------------------------------------------------------------
+--        Script Postgre
+------------------------------------------------------------
 
 
-#------------------------------------------------------------
-# Table: equipe
-#------------------------------------------------------------
 
-CREATE TABLE equipe(
-        e_id   int (11) Auto_increment  NOT NULL ,
-        e_name Varchar (25) ,
-        e_etat Varchar (2) ,
-        PRIMARY KEY (e_id )
-)ENGINE=InnoDB;
+------------------------------------------------------------
+-- Table: equipe
+------------------------------------------------------------
+CREATE TABLE public.equipe(
+	e_id   SERIAL NOT NULL ,
+	e_name VARCHAR (25)  ,
+	e_etat VARCHAR (25)  ,
+	CONSTRAINT prk_constraint_equipe PRIMARY KEY (e_id)
+)WITHOUT OIDS;
 
 
-#------------------------------------------------------------
-# Table: objet
-#------------------------------------------------------------
+------------------------------------------------------------
+-- Table: objet
+------------------------------------------------------------
+CREATE TABLE public.objet(
+	o_id     SERIAL NOT NULL ,
+	o_name   VARCHAR (25)  ,
+	o_found  BOOL   ,
+	o_coordx VARCHAR (25)  ,
+	o_coordy VARCHAR (25)  ,
+	o_image  VARCHAR (25)  ,
+	e_id     INT   ,
+	CONSTRAINT prk_constraint_objet PRIMARY KEY (o_id)
+)WITHOUT OIDS;
 
-CREATE TABLE objet(
-        obj_id  int (11) Auto_increment  NOT NULL ,
-        o_name  Varchar (25) ,
-        o_found Bool ,
-        o_coordx Varchar (25) ,
-        o_coordy Varchar (25) ,
-        e_id    Int ,
-        PRIMARY KEY (obj_id )
-)ENGINE=InnoDB;
+ALTER TABLE public.objet ADD CONSTRAINT FK_objet_e_id FOREIGN KEY (e_id) REFERENCES public.equipe(e_id);
 
-ALTER TABLE objet ADD CONSTRAINT FK_objet_e_id FOREIGN KEY (e_id) REFERENCES equipe(e_id);
 
 INSERT INTO `equipe` (`e_id`, `e_name`) VALUES (NULL, 'Flo', 'c');
 INSERT INTO `equipe` (`e_id`, `e_name`) VALUES (NULL, 'Jer', 'c');
